1b3ad8e36f465a1ed606a8573017423c5ed2a9ad
Seemant <seemantbishnoi02@gmail.com>
2021-07-31 11:58:22 +0530

    changes for changed structure of imdb

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from requests import get
 from bs4 import BeautifulSoup
 
+
 def scrape_movie(movie_name):
     ##### For Fetching Link of First Title From Results ######
     url = "https://www.imdb.com/find?q="+movie_name+"&ref_=nv_sr_sm"
@@ -9,88 +10,70 @@ def scrape_movie(movie_name):
     result = html_soup.find('td', class_='result_text')
 
     ##### For Scraping Movie Info ######
+    #####print(result.a['href'])
 
-    movie_url = 'https://www.imdb.com'+result.a['href']
+    movie_url = 'https://www.imdb.com'+result.a['href']+'?ref_=fn_al_tt_1'
     response = get(movie_url)
     html_soup = BeautifulSoup(response.text, 'html.parser')
 
-    movieInfo = {'title': '', 'rating': '', 'duration': '', 'categories': [], 'summary': '',
-                 'directors': [], 'writers': [], 'stars': [], 'casts': [], 'storyline': '', 'genres': []}
+    movieInfo = {'title': '', 'rating': '', 'categories': [], 'summary': '',
+                 'directors': [], 'stars': []}
     ##### For Scraping Title ######
 
-    title = html_soup.find('h1').text
+    title = html_soup.find('h1', class_ = 'TitleHeader__TitleText-sc-1wu6n3d-0 dxSWFG').text
     movieInfo['title'] = title.replace('\xa0', ' ')
-
+    ###print(movieInfo['title'])
     ##### For Scraping Rating ######
 
-    rating = html_soup.find('span', itemprop='ratingValue').text
+    rating = html_soup.find('span', class_='AggregateRatingButton__RatingScore-sc-1ll29m0-1 iTLWoV').text
+    ##print(rating)
     movieInfo['rating'] = rating+"/10"
 
-    ##### For Scraping Duration ######
+    plot = html_soup.find('div', class_='ipc-html-content ipc-html-content--base').find_all('div')
 
-    duration = html_soup.find('time').text.strip()
-    movieInfo['duration'] = duration
+    summary = plot[0].text.strip()
+
+    ##print(summary)
+    movieInfo['summary'] = summary
 
     ##### For Scraping Category ######
 
-    categories_data = html_soup.find('div', class_='subtext').find_all('a')
+    categories_data = html_soup.find('div', class_='ipc-chip-list GenresAndPlot__GenresChipList-cum89p-4 gtBDBL').find_all('a')
     categories_data.pop()
     categories = []
     for category in categories_data:
+        ###print(category.text)
         categories.append(category.text)
 
     movieInfo['categories'] = categories
 
     ##### For Scraping Summary ######
 
-    plot = html_soup.find('div', class_='plot_summary').find_all('div')
-
-    summary = plot[0].text.strip()
-    movieInfo['summary'] = summary
+    
 
     ##### For Scraping List of Directors ######
-    directors = []
-    for director in plot[1].find_all('a'):
-        directors.append(director.text)
 
+    directors_data = html_soup.find('li', class_ = 'ipc-metadata-list__item').find_all('div')
+    
+    directors = []
+    for director in directors_data:
+        ###print(director.text)
+        directors.append(director.text)
+    directors.pop();
     movieInfo['directors'] = directors
 
     ##### For Scraping List of Writers ######
-    writers = []
-    for writer in plot[2].find_all('a'):
-        writers.append(writer.text)
-    movieInfo['writers'] = writers
 
-    ##### For Scraping List of Stars ######
+    #### For Scraping List of Stars ######
+    stars_data = html_soup.find('div', class_ = 'ipc-sub-grid ipc-sub-grid--page-span-2 ipc-sub-grid--wraps-at-above-l title-cast__grid').find_all('div',class_ = 'StyledComponents__CastItemWrapper-y9ygcu-7 hTEaNu')
     stars = []
-    for star in plot[3].find_all('a'):
-        stars.append(star.text)
-    stars.pop()
+    for star in stars_data:
+        ##print(star.find_all('div', class_= 'StyledComponents__CastItemSummary-y9ygcu-9 fBAofn')[0].a.text)
+        stars.append(star.find_all('div', class_= 'StyledComponents__CastItemSummary-y9ygcu-9 fBAofn')[0].a.text)
+
     movieInfo['stars'] = stars
 
-    ###### For Scraping Cast ######
-    casts_html = html_soup.find('table', class_='cast_list').find_all('tr')
-    casts_html.pop(0)
-
-    casts = []
-    for cast_html in casts_html:
-        casts.append(cast_html.find_all('td')[1].a.text.replace("\n", ""))
-    movieInfo['casts'] = casts
-
-    ##### For Scraping Storyline ######
-
-    storyline = html_soup.find(
-        'div', class_='inline canwrap').p.span.text.strip()
-    movieInfo['storyline'] = storyline
-
-    ######## For Scraping Genres #######
-    genres_html = html_soup.find_all(
-        'div', class_='see-more inline canwrap')[1].find_all('a')
-    genres = []
-
-    for genre_html in genres_html:
-        genres.append(genre_html.text)
-    movieInfo['genres'] = genres
 
     ########## FINAL RESULT ###########
     return movieInfo
+  
